a3ccb1c538a4d89b7eb54450f3121a16e6e3c247
calebkim107 <calebkim1117@gmail.com>
2020-08-03 20:52:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+print("Hello World")
+
+# VARIABLE - a way to store a single piece of data.
+# ex: Score and Health in games.
+
+#nameofvariable = value
+Score = 0
+name = "Spongebob"
+health = 100.405679
+
+print(name)
+
+#CONDITIONALS - if/else statements used for decision-making
+
+if Score == 5:
+  print("your score is 5")
+else:
+  print("score is 0")
+num = 30
+  
+
+  # Create an if/else statement that will print
+# whether num is less than or greater than 30.
+if num == 30:
+  print ("greater than 30")
+elif num == 30:
+ print ("num equals 30")
+else:
+  print("num equals 30")
+
+# LOOPS: a way to repeat code for a given mount of times or until a condition is met
+
+for i in range(5):
+  print("smellycat")
+
+count = 0 
+while (count < 9):
+  print("The count is:" + str(count))
+# count = count + 1
+  count += 1
+
+
+
+name = input("What is your name")
+print("Hello" + name)
+
+
+
+
+avengers = ("black widow", "iron man")
+
+
+
+avengers_count = 4
+
+
+
+
+num1 = int(input("Please enter a number."))
+num2 = int(input("Please enter another number."))
+print("The sum is: {}".format(num1+num2))
+
+print("Hello")
+age = input("How old are you?")
+fullName = input("What is your first and last name?")
+activity = input("What is your favorite activity?")
+
+print("Your name is" + fullName)
+print("You are" + age + " years old")
+print("Your favorite activity is: "+ activity)
+
+
+
+
+
+
+
+
+
+
